a7845440b883ae14cd85ecf82fdeff09a9130417
MonteDeveloper <lorenzomontello83@gmail.com>
2023-07-18 15:53:24 +0200

    add project image

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -14,6 +14,7 @@ export default {
 <template>
     <div class="card text-white bg-dark" style="max-width: 18rem;">
         <div class="card-body">
+            <img :src="project.image ? `http://localhost:8000/storage/${project.image}` : 'http://localhost:8000/storage/placeholders/placeholder.png'" class="card-img-top" :alt="project.name">
             <h5 class="card-title text-uppercase">{{ project.name }}</h5>
             <p class="card-text">
                 {{ project.description }}
